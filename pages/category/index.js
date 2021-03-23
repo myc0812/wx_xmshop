814d13942e9d1fdfa461f486d1152ae819f4eeee
myc0812 <mayichai123@gmail.com>
2021-03-23 13:43:14 +0800

    完成分类页面

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,73 @@
-// pages/category/index.js
+import {
+  request
+} from "../../request/index.js";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 左侧导航栏数据
+    leftMenuList: [],
+    // 右侧商品数据
+    rightContent: [],
+    // 被点击的左侧菜单
+    currentIndex: 0,
+    // 距离顶部的距离
+    scrollTop: 0
   },
+  // 接口的返回数据
+  Cates: [],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad(options) {
+    // 获取本地存储的数据
+    const Cates = wx.getStorageSync('cates')
+    // 判断
+    if (!Cates) {
+      this.getCates()
+    } else {
+      if (Date.now() - Cates.time > (1000 * 10)) {
+        this.getCates()
+      } else {
+        this.Cates = Cates.data
+        let leftMenuList = this.Cates.map(v => v.cat_name)
+        let rightContent = this.Cates[0].children
 
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
+  // 获取分类数据
+  async getCates() {
+    const res = await request({url: "/categories"})
+    this.Cates = res.data.message;
+    //将数据存入到本存储中
+    wx.setStorageSync('cates', {
+      time: Date.now(),
+      data: this.Cates
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    let leftMenuList = this.Cates.map(v => v.cat_name)
+    let rightContent = this.Cates[0].children
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    this.setData({
+      leftMenuList,
+      rightContent
+    })
+},
+// 左侧菜单的点击事件
+handleItemTap(e)
+{
+  const {
+    index
+  } = e.target.dataset
+  let rightContent = this.Cates[index].children
+  this.setData({
+    currentIndex: index,
+    rightContent,
+    // 重新设置view-scorll的距离顶部距离
+    scrollTop: 0
+  })
+}
 })
+;
--- pages/index/index.js
+++ pages/index/index.js
@@ -24,7 +24,7 @@ Page({
   // 获取轮播图数据
   getSwiperList() {
     request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+      url: '/home/swiperdata'
     }).then(result => {
       this.setData({
         swiperList: result.data.message
@@ -35,7 +35,7 @@ Page({
   // 获取分类导航数据
   getCateList() {
     request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+      url: '/home/catitems'
     }).then(result => {
       this.setData({
         cateList: result.data.message
@@ -46,7 +46,7 @@ Page({
   // 获取楼层数据
   getFloorList() {
     request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+      url: '/home/floordata'
     }).then(result => {
       this.setData({
         floorList: result.data.message
