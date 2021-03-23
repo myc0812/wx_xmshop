0d463148d303b53ef71fa787c348b7d7131ab003
myc0812 <mayichai123@gmail.com>
2021-03-23 19:46:05 +0800

    优化动态渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,43 @@
 // pages/goods_detail/index.js
+import {
+  request
+} from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {
+      goods_id
+    } = options
+    this.getGoodsDetail(goods_id)
   },
+  // 获取商品详情数据
+  async getGoodsDetail(goods_id) {
+    const res = await request({
+      url: '/goods/detail',
+      data: {
+        goods_id
+      }
+    })
+    this.setData({
+      goodsObj: {
+        goods_name: res.data.message.goods_name,
+        goods_price: res.data.message.goods_price,
+        // 苹果手机部分不识别webp格式
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        goods_introduce: res.data.message.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: res.data.message.pics
+      }
+    })
   }
 })
